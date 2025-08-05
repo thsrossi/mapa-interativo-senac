f42b50b96495be0cac7fb427d7ff7601076294cb
thsrossi <matheusbprossi@gmail.com>
2025-08-05 01:48:19 -0300

    remove img mapper prod

--- interactive-map.tsx
+++ interactive-map.tsx
@@ -119,7 +119,7 @@ export default function InteractiveMap() {
             </div>
           )}
 
-          <img
+          {/* <img
             src={imagemMapa.src}
             alt="Planta Baixa"
             className="max-w-none w-[1200px] h-[800px] object-contain"
@@ -131,7 +131,7 @@ export default function InteractiveMap() {
               const y = ((e.clientY - rect.top) / rect.height) * 100;
               console.log(`x: ${x.toFixed(2)}%, y: ${y.toFixed(2)}%`);
             }}
-          />
+          /> */}
         <TransformWrapper
             ref={transformComponentRef}
             initialScale={isMobile ? 0.7 : 1}
@@ -157,7 +157,7 @@ export default function InteractiveMap() {
               contentClass="w-full h-screen flex items-center justify-center"
             >
               <div className="relative inline-block z-20 py-16">
-                {/* <img
+                <img
                   src={imagemMapa.src}
                   alt="Planta Baixa"
                   className="max-w-none w-[1200px] h-[800px] object-contain"
@@ -171,7 +171,7 @@ export default function InteractiveMap() {
                   }}
                   onLoad={() => setImageLoaded(true)}
 
-                /> */}
+                />
 
                 {imageLoaded && markers.filter(m => m.visible).map((marker) => (
                   <Tooltip key={marker.id}>
