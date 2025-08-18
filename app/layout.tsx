190805e98687a3a0471aae0ca610ef479c0c563b
thsrossi <matheusbprossi@gmail.com>
2025-08-18 20:45:53 -0300

    inserido GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Mapa Interativo - Senac PR - Unidade Curitiba Centro',
@@ -18,6 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MKCKXN5K');
+            `,
+          }}
+        />
         <link rel="shortcut icon" href="/favicon.ico"></link>
         <style>{`
 html {
@@ -27,7 +42,16 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body><noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MKCKXN5K"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+        {children}
+      </body>
     </html>
   )
 }
